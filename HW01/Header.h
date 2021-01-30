22c86bd61941eeb7c165c471872a59d9e160426d
Enigma Swan Adams <eswanadams@alaska.edu>
2021-01-29 22:35:22 -0900

    additional clarification

--- HW01/Header.h
+++ HW01/Header.h
@@ -6,7 +6,7 @@ each program for the homewrok is described above its .cpp file
 this program just allows there main fucntions to work
 **********************************************/
 
-#pragma once
-int diamond();
+#pragma once // each function here does everything for its respective program
+int diamond(); 
 int greatest();
 int mileskm();
